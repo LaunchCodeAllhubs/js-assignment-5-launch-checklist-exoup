4818b4e9a5d8f17aa2d1cb1fa2c993c42b579c2c
exoup <JosephY@dram.space>
2023-08-01 18:37:26 -0500

    Creating data

--- script.js
+++ script.js
@@ -2,15 +2,31 @@
 
 window.addEventListener("load", function() {
 
+    let form = document.querySelector('form[data-testid="testForm"');
+    let list = document.getElementById("faultyItems");
+
+    form.addEventListener('submit', (e) => {
+        
+        e.preventDefault(); //no submit
+        
+        let pilotValue = document.querySelector('input[name="pilotName"').value;
+        let copilotValue = document.querySelector('input[name="copilotName"').value;
+        let fuelLevelValue = document.querySelector('input[name="fuelLevel"').value;
+        let cargoLevelValue = document.querySelector('input[name="cargoMass"').value;
+        
+        formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue);
+        
+    });
+
+
    let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
-       console.log(listedPlanets);
    }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+       let chosenPlanet = pickPlanet(listedPlanets);
+       //function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl)
+       addDestinationInfo(document, chosenPlanet.name, chosenPlanet.diameter, chosenPlanet.star, chosenPlanet.distance, chosenPlanet.moons, chosenPlanet.image);
+   });
    
 });
